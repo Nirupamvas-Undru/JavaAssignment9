a657774aca93b22dca15ca3194ebd827649b2a48
Nirupamvas-Undru <nirupamvas.undru@zemosolabs.com>
2022-03-21 13:58:38 +0530

    Assignment 9 files

--- RegularExpression.java
+++ RegularExpression.java
@@ -1,7 +1,10 @@
 import java.util.Formatter;
 import java.util.regex.*;
 public class RegularExpression {
+    //inputString variable
     private String inputString = "This is a sample test sentence.";
+
+    //parse method to check weather the given input string matches our regular expression
     public void parse(String regex){
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher;
@@ -9,9 +12,12 @@ public class RegularExpression {
         matcher = pattern.matcher(inputString);
         f.format("%1$-15.15s %2$-8.8s\n", inputString, matcher.find());
     }
+
     public static void main(String[] args) {
         RegularExpression object = new RegularExpression();
+        //regex pattern
         String regex ="^[A-Z].*\\.";
+        //function call
         object.parse(regex);
     }
 }
